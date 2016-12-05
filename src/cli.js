7bfa836dd89ce73d7b4c6bb8f6f78557d8ff73be
Abimbola Idowu <abimbola2010@gmail.com>
2016-12-05 13:38:44 +0100

    feat(CLI): add basic implementation

---
+++ src/cli.js
@@ -0,0 +1,147 @@
+import fs from 'fs'
+import log from 'npmlog'
+import PrettyError from 'pretty-error'
+import yargs from 'yargs'
+
+import CONSTANTS from './constants'
+import LineItemStateCsvParser from './lineitemstate-csv-parser'
+import getApiCredentials from './get-api-credentials'
+import { version } from '../package.json'
+
+process.title = 'csvparserorders'
+
+const args = yargs
+  .usage(
+    `\n
+Usage: $0 [options]
+Convert commercetools orders CSV data to JSON.`
+  )
+  .showHelpOnFail(false)
+
+  .option('help', {
+    alias: 'h',
+  })
+  .help('help', 'Show help text.')
+
+  .option('version', {
+    alias: 'v',
+    type: 'boolean',
+  })
+  .version('version', 'Show version number.', () => version)
+  .option('type', {
+    alias: 't',
+    choices: ['lineitemstate'],
+    describe: 'Predefined type of csv',
+    demand: true,
+  })
+  .option('inputFile', {
+    alias: 'i',
+    default: 'stdin',
+    describe: 'Path to CSV file.',
+  })
+  .coerce('inputFile', (arg) => {
+    if (arg !== 'stdin')
+      return fs.createReadStream(String(arg))
+
+    return process.stdin
+  })
+
+  .option('outputFile', {
+    alias: 'o',
+    default: 'stdout',
+    describe: 'Input CSV file.',
+  })
+  .coerce('outputFile', (arg) => {
+    if (arg !== 'stdout')
+      return fs.createWriteStream(String(arg))
+
+    // No output file given, log to file to not disturb stdout/stderr
+    log.stream = fs.createWriteStream('csvparserprice.log')
+
+    return process.stdout
+  })
+
+  .option('batchSize', {
+    alias: 'b',
+    default: CONSTANTS.standardOption.batchSize,
+    describe: 'Number of CSV rows to handle simultaneously.',
+  })
+
+  .option('delimiter', {
+    alias: 'd',
+    default: CONSTANTS.standardOption.delimiter,
+    describe: 'Used CSV delimiter.',
+  })
+
+  .option('strictMode', {
+    alias: 's',
+    default: CONSTANTS.standardOption.strictMode,
+    describe: 'Parse CSV strictly.',
+  })
+
+  .option('projectKey', {
+    alias: 'p',
+    describe: 'API project key.',
+    demand: true,
+  })
+
+  .option('host', {
+    describe: 'HTTP client host parameter.',
+  })
+
+  .option('protocol', {
+    describe: 'HTTP client protocol parameter.',
+  })
+
+  .option('accessToken', {
+    describe: 'HTTP client access token.',
+  })
+
+  .option('logLevel', {
+    default: 'info',
+    describe: 'Logging level: error, warn, info or verbose.',
+  })
+  .coerce('logLevel', (arg) => {
+    log.level = arg
+  })
+  .argv
+
+const errorHandler = (error) => {
+  const errorFormatter = new PrettyError()
+  let formattedError
+  if (log.level === 'verbose')
+    formattedError = errorFormatter.render(error)
+  else
+    formattedError = error.message
+
+  process.stderr.write(formattedError)
+  process.stderr.write('\n')
+  log.error('', formattedError)
+  process.exit(1)
+}
+
+getApiCredentials(args.projectKey, args.accessToken)
+  .then((apiCredentials) => {
+    if (args.type === 'lineitemstate')
+      return new LineItemStateCsvParser(
+        {
+          config: apiCredentials,
+          host: args.host,
+          protocol: args.protocol,
+          access_token: args.accessToken,
+        },
+        {
+          error: errorHandler,
+          warn: message => log.warn('', message),
+          info: message => log.info('', message),
+          verbose: message => log.verbose('', message),
+        },
+        {
+          delimiter: args.delimiter,
+        }
+      )
+    return Promise.reject('Invalid csv type')
+  })
+  .then((module) => {
+    module.parse(args.inputFile, args.outputFile)
+  })
---
+++ test/integration/cli.spec.js
@@ -0,0 +1,153 @@
+import { exec } from 'child_process'
+import fs from 'fs'
+import test from 'tape'
+import tmp from 'tmp'
+
+import { version } from '../../package.json'
+
+const binPath = './bin/csvparserorders.js'
+
+let PROJECT_KEY
+if (process.env.CI === 'true')
+  PROJECT_KEY = process.env.SPHERE_PROJECT_KEY
+else
+  PROJECT_KEY = process.env.npm_config_projectkey
+
+test('CLI help flag', (t) => {
+  exec(`${binPath} --help`, (error, stdout, stderr) => {
+    t.true(String(stdout).match(/help/), 'outputs help text')
+    t.false(error && stderr, 'returns no error')
+    t.end()
+  })
+})
+
+test('CLI version flag', (t) => {
+  exec(`${binPath} --version`, (error, stdout, stderr) => {
+    t.equal(stdout, `${version}\n`, 'outputs current version number')
+    t.false(error && stderr, 'returns no error')
+    t.end()
+  })
+})
+
+test('CLI takes input from file', (t) => {
+  const csvFilePath = './test/helpers/lineitemstate-sample.csv'
+
+  exec(
+    `${binPath} -p ${PROJECT_KEY} -t lineitemstate --inputFile ${csvFilePath}`,
+    (error, stdout, stderr) => {
+      const expectedOutput = [{
+        orderNumber: '234',
+        lineItems: [{
+          state: [{
+            quantity: 10,
+            state: {
+              id: 'shipped',
+            },
+          }],
+        }],
+      }]
+      t.false(error && stderr, 'returns no error')
+      t.deepEqual(
+        JSON.parse(stdout),
+        expectedOutput,
+        'lineitemstate csv parser '
+      )
+      t.end()
+    }
+  )
+})
+
+test('CLI writes output to file', (t) => {
+  const csvPath = './test/helpers/lineitemstate-sample.csv'
+  const output = tmp.fileSync().name
+  const expectedOutput = [{
+    orderNumber: '234',
+    lineItems: [{
+      state: [{
+        quantity: 10,
+        state: {
+          id: 'shipped',
+        },
+      }],
+    }],
+  }]
+  exec(
+    `${binPath} -p ${PROJECT_KEY} -i ${csvPath} -o ${output} -t lineitemstate`,
+    (cliError, stdout, stderr) => {
+      t.false(cliError && stderr, 'returns no CLI error')
+
+      fs.readFile(output, { encoding: 'utf8' }, (error, data) => {
+        t.deepEqual(
+          JSON.parse(data),
+          expectedOutput,
+          'lineitemstate csv parser outputs data to a file'
+        )
+        t.false(error, 'returns no FS error')
+        t.end()
+      })
+    }
+  )
+})
+
+test('CLI given a non-existant input file', (t) => {
+  exec(`${binPath} -t lineitemstate -p ${PROJECT_KEY} -i nope.csv`, (error) => {
+    t.true(error, 'returns error when csv file is not present')
+    t.end()
+  })
+})
+
+test('CLI exits on faulty CSV format', (t) => {
+  const csvFile = './test/helpers/faulty-sample.csv'
+  const output = tmp.fileSync().name
+  const type = 'lineitemstate'
+
+  exec(`${binPath} -p ${PROJECT_KEY} -i ${csvFile} -t ${type} -o ${output}`,
+    (error, stdout, stderr) => {
+      t.equal(error.code, 1, 'returns process error exit code')
+      t.false(stdout, 'returns no stdout data')
+      t.true(
+        stderr.match(/Row length does not match headers/),
+        'returns CSV parsing error on stderr'
+      )
+      t.end()
+    }
+  )
+})
+
+test('CLI logs stack trace on verbose level', (t) => {
+  const csvFile = './test/helpers/faulty-sample.csv'
+  const type = 'lineitemstate'
+  const logLevel = 'verbose'
+
+  exec(
+    // eslint-disable-next-line
+    `${binPath} -p ${PROJECT_KEY} -i ${csvFile} -t ${type} --logLevel ${logLevel}`,
+    (error, stdout, stderr) => {
+      t.equal(error.code, 1, 'returns process error exit code')
+      t.false(stdout, 'returns no stdout data')
+      t.true(
+        stderr.match(/Row length does not match headers/),
+        'returns CSV parsing error on stderr'
+      )
+      t.end()
+    }
+  )
+})
+
+test('CLI should return error if invalid type is passed', (t) => {
+  const csvFile = './test/helpers/faulty-sample.csv'
+  const type = 'invalid'
+
+  exec(
+    `${binPath} -p ${PROJECT_KEY} -i ${csvFile} -t ${type}`,
+    (error, stdout, stderr) => {
+      t.ok(error)
+      t.false(stdout, 'returns no stdout data')
+      t.true(
+        stderr.match(/Invalid values/),
+        'CLI module returns error when invalid type is given'
+      )
+      t.end()
+    }
+  )
+})
